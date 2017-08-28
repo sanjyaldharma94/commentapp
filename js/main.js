49390fd5f296b462095a68dcf594d01e53bcd446
Dharma Sanjyal <rajsan44@gmail.com>
2017-08-28 12:38:02 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function display() {
+	var userInput = document.getElementById("userName").value;
+	var userComment = document.getElementById("userComment").value;
+	console.log(userInput);
+	console.log(userComment);
+ var text = document.createTextNode("Username: " +  userInput);
+ var message = document.createTextNode("Comment: " + userComment);
+ 
+ var newListItem = document.createElement("LI")
+	newListItem.className = "list-group-item"
+	var newUserName = document.createElement("H4")
+	newUserName.appendChild(text)
+	var newComment = document.createElement("P")
+	newComment.appendChild(message)
+
+	newListItem.appendChild(newUserName)
+	newListItem.appendChild(message)
+	document.getElementById("commentList").appendChild(newListItem)
+}
