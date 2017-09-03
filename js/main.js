a8ae77c230d88187d00b333f0973b1ef69b6896a
Dharma Sanjyal <rajsan44@gmail.com>
2017-09-03 12:01:52 -0700

    third Commit

--- js/main.js
+++ js/main.js
@@ -1,19 +1,54 @@
 function display() {
 	var userInput = document.getElementById("userName").value;
 	var userComment = document.getElementById("userComment").value;
-	console.log(userInput);
-	console.log(userComment);
+
  var text = document.createTextNode("Username: " +  userInput);
  var message = document.createTextNode("Comment: " + userComment);
  
 var newListItem = document.createElement("LI");
 	newListItem.className = "list-group-item";
+
 var newUserName = document.createElement("H4");
 	newUserName.appendChild(text);
+
 var newComment = document.createElement("P");
 	newComment.appendChild(message);
-
+  
 	newListItem.appendChild(newUserName);
 	newListItem.appendChild(message);
 	document.getElementById("commentList").appendChild(newListItem);
+    //creating a new button on click of submit button
+ 	var clearBtn = document.createElement("button");
+ 	// random function to run when a button is clicked
+ function clearBtnFunction() {
+ 	console.log("this button functio ran!");
+ };
+ //Assingning values , class, type to the button
+     clearBtn.className = "btn btn-large btn-default";
+ 
+    clearBtn.textContent = "Clear All";
+    clearBtn.type = "button";
+    clearBtn.id = "clear";
+   //appending button to the body
+document.body.appendChild(clearBtn);
+//loading up addEventListener to the button so that it functions when clicked
+clearBtn.addEventListener('click',clearBtnFunction, false);
+ function clearBtnFunction() {
+ 	alert("Do you want to permanently delete the comments?");
+ 	document.getElementById("commentList").removeChild(newListItem);
+ 	document.body.removeChild(clearBtn);
+
+  
+ }
+ 
 }
+
+
+
+
+
+
+
+
+
+
---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("clearBtn").click(function(){
+        $("#commentList").remove();
+    });
+});
