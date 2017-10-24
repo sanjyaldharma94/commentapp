652a8c81113c301e2b71d5135cf3e401076fd01f
Dharma Sanjyal <rajsan44@gmail.com>
2017-10-24 13:07:27 -0700

    third commit

--- js/main.js
+++ js/main.js
@@ -19,23 +19,30 @@ var newComment = document.createElement("P");
 	document.getElementById("results").appendChild(newListItem);
     //creating a new button on click of submit button
  	var clearBtn = document.createElement("button");
+
  	// random function to run when a button is clicked
+ 	if(userInput == "" && userComment == "") {
+ 		alert("Please type your name");
+ 		document.getElementById("results").appendChild("");
+ 	}
  function clearBtnFunction() {
  	console.log("this button function ran!");
  };
  //Assingning values , class, type to the button
-     clearBtn.className = "btn btn-large btn-default";
+     clearBtn.className = "btn btn-large btn-warning";
  
     clearBtn.textContent = "Clear All";
     clearBtn.type = "button";
     clearBtn.id = "clear";
    //appending button to the body
-document.body.appendChild(clearBtn);
+   document.getElementById("results").appendChild(clearBtn);
+//document.body.appendChild(clearBtn);
 //loading up addEventListener to the button so that it functions when clicked
 clearBtn.addEventListener('click', clearBtnFunction, false);
  function clearBtnFunction() {
  	alert("Do you want to permanently delete the comments?");
  	document.getElementById("results").removeChild(newListItem);
+ 	document.getElementById("results").removeChild(clearBtn);
  	document.body.removeChild(clearBtn);
 
  
