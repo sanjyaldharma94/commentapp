41b4558a00bc7b446c0558721355104f47c4f9fd
Dharma Sanjyal <rajsan44@gmail.com>
2017-11-13 11:38:44 -0800

    changed display

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 function display() {
+	//saving user input data
 	var userInput = document.getElementById("userName").value;
 	var userComment = document.getElementById("userComment").value;
 
  var text = document.createTextNode("Username: " +  userInput);
  var message = document.createTextNode("Comment: " + userComment);
- 
-var newListItem = document.createElement("LI");
+
+var newListItem = document.createElement("li");
 	newListItem.className = "list-group-item";
 
 var newUserName = document.createElement("H4");
@@ -13,49 +14,36 @@ var newUserName = document.createElement("H4");
 
 var newComment = document.createElement("P");
 	newComment.appendChild(message);
-  
+
 	newListItem.appendChild(newUserName);
 	newListItem.appendChild(message);
 	document.getElementById("results").appendChild(newListItem);
     //creating a new button on click of submit button
  	var clearBtn = document.createElement("button");
+	//Assingning values , class, type to the button
+			clearBtn.className = "btn btn-large btn-warning";
 
- 	// random function to run when a button is clicked
- 	if(userInput == "" && userComment == "") {
- 		alert("Please type your name");
- 		document.getElementById("results").appendChild("");
- 	}
- function clearBtnFunction() {
- 	console.log("this button function ran!");
- };
- //Assingning values , class, type to the button
-     clearBtn.className = "btn btn-large btn-warning";
- 
-    clearBtn.textContent = "Clear All";
-    clearBtn.type = "button";
-    clearBtn.id = "clear";
-   //appending button to the body
-   document.getElementById("results").appendChild(clearBtn);
-//document.body.appendChild(clearBtn);
+		 clearBtn.textContent = "Clear All";
+		 clearBtn.type = "button";
+		 clearBtn.id = "clear";
+		//appending button to the body
+		document.getElementById("results").appendChild(clearBtn);
+
 //loading up addEventListener to the button so that it functions when clicked
 clearBtn.addEventListener('click', clearBtnFunction, false);
+
+if(userInput == "" && userComment == "") {
+	alert("Please type your name");
+	document.getElementById("results").appendChild("");
+}
+function clearBtnFunction() {
+console.log("this button function ran!");
+};
  function clearBtnFunction() {
  	alert("Do you want to permanently delete the comments?");
  	document.getElementById("results").removeChild(newListItem);
  	document.getElementById("results").removeChild(clearBtn);
- 	document.body.removeChild(clearBtn);
-
- 
- }
- 
-}
-
-
-
-
-
-
-
-
-
-
+ 	//document.body.removeChild(clearBtn);
+	//form reset();
+};
+ };
